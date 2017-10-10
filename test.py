f832e0b0ac7d94406475f234d59f86085cb836e9
luojiebin <luo.jiebin@qq.com>
2017-10-10 23:46:13 +0800

    for problem test

---
+++ test.py
@@ -0,0 +1,12 @@
+"""
+ID: luojiebin
+LANG: PYTHON
+TASK: test
+"""
+
+fin = open('test.in', 'r')
+fout = open('test.out', 'w')
+x, y = map(int, fin.readline().split())
+sum = x + y
+fout.write(str(sum) + '\n')
+fout.close()
